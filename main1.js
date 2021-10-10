7e433a74f104005e537f69d19e86cb30f2208fe4
Saumil <90200635+saumil2306@users.noreply.github.com>
2021-10-10 13:38:49 +0530

    Create main1.js

---
+++ main1.js
@@ -0,0 +1,148 @@
+let carts = document.querySelectorAll('.add-cart');
+
+let products = [
+    {
+        name: "Nike Jordan's",
+        tag: 'Jordans',
+        price: 12000,
+        inCart: 0
+    },
+    {
+        name: 'Nike Kalenji',
+        tag: 'Kalenji',
+        price: 7000,
+        inCart: 0
+    },
+    {
+        name: 'Adidas FORUM',
+        tag: 'FORUM',
+        price: 9999,
+        inCart: 0
+    },
+    {
+        name: 'Adidas Fluidstreet',
+        tag: 'Fliudstreet',
+        price: 3599,
+        inCart: 0
+    }
+
+];
+
+for (let i = 0; i < carts.length; i++) {
+    carts[i].addEventListener('click', () => {
+        cartNumbers(products[i]);
+        totalCost(products[i]);
+
+    })
+
+}
+
+function onLoadCartNumbers() {
+    let productNumbers = localStorage.getItem('cartNumbers');
+    if (productNumbers) {
+        document.querySelector('.cart span').textContent = productNumbers;
+    }
+}
+
+function cartNumbers(product) {
+
+    let productNumbers = localStorage.getItem('cartNumbers');
+    productNumbers = parseInt(productNumbers);
+    if (productNumbers) {
+        localStorage.setItem('cartNumbers', productNumbers + 1)
+        document.querySelector('.cart span').textContent = productNumbers + 1;
+    }
+    else {
+        localStorage.setItem('cartNumbers', 1);
+        document.querySelector('.cart span').textContent = 1;
+    }
+
+    setItems(product);
+}
+
+function setItems(product) {
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
+
+    if (cartItems != null) {
+
+        if (cartItems[product.tag] == undefined) {
+            cartItems = {
+                ...cartItems,
+                [product.tag]: product
+
+            }
+
+        }
+        cartItems[product.tag].inCart += 1;
+    }
+    else {
+        product.inCart = 1;
+        cartItems = {
+            [product.tag]: product
+        }
+
+    }
+
+    localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+
+}
+
+function totalCost(product) {
+    let cartCost = localStorage.getItem('totalCost');
+
+    if (cartCost != null) {
+        cartCost = parseInt(cartCost);
+        localStorage.setItem("totalCost", cartCost + product.price);
+    }
+    else {
+        localStorage.setItem("totalCost", product.price);
+    }
+
+}
+
+function displayCart(){
+    let cartItems = localStorage.getItem("productsInCart");
+    cartItems = JSON.parse(cartItems);
+    let productContainer = document.querySelector(".products");
+    let productnull = document.querySelector(".productnull");
+    let cartCost = localStorage.getItem('totalCost');
+    if( cartItems && productContainer ){
+        
+        productContainer.innerHTML = '';
+        Object.values(cartItems).map(item =>{
+            productContainer.innerHTML += `
+            <div class="products" style="width: 250px; display:-webkit-box; margin-bottom: 23px; border-bottom: 2px solid lightgrey;">
+            <img src="imgs/${item.tag}.webp" style="width: 250px;">
+            <span>${item.name}</span>
+            <div class="price" style="width: 100px; display:flex; align-items: center; justify-content: center;border-bottom: 2px solid lightgrey; margin-bottom: -2px;">${item.price}</div>
+            <div class="quantity" style="width: 135px; display:flex; align-items: center; justify-content: center;border-bottom: 2px solid lightgrey; margin-bottom: -2px;">${item.inCart}</div>
+            <div class="total" style="width: 230px; display:flex; align-items: center; justify-content: center;border-bottom: 2px solid lightgrey; margin-bottom: -2px;">${item.inCart * item.price}</div>
+            </div>
+
+            ` 
+        });
+
+        productContainer.innerHTML += `
+        
+        <div class="basketTotalContainer">
+            <h4 class="basketTotalTitle" style="display: flex;justify-content: end;">
+                Cart total Rs. ${cartCost}.00
+            </h4>
+        </div>
+
+        `
+
+    }
+    else{
+        productnull.innerHTML = `
+        <div class="productnull">
+        <h2 style="display: flex;justify-content: center;margin-top: 23px;margin-bottom: 23px;">Your cart is empty</h2>
+        </div>
+        `
+    }
+}
+
+
+onLoadCartNumbers();
+displayCart();
